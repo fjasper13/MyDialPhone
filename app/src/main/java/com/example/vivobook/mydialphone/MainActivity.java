1733eaa1cdeccd992c21c49bc16e5901b60d1f12
fjasper13 <fulgentiusjasper@gmail.com>
2019-02-22 14:06:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/vivobook/mydialphone/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.vivobook.mydialphone;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText=findViewById(R.id.editText_main);
+        if(editText!=null){
+            editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    boolean handled=false;
+                    if (actionId== EditorInfo.IME_ACTION_SEND){
+                        dialNumber();
+                        handled=true;
+                    }
+                    return handled;
+                }
+            });
+        }
+    }
+    private void dialNumber(){
+        EditText editText=findViewById(R.id.editText_main);
+        String phoneNum=null;
+        if(editText!=null) phoneNum="tel:"+editText.getText().toString();
+        Log.d(TAG, "dialNumber: " + phoneNum);
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+
+        intent.setData(Uri.parse(phoneNum));
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
+    }
+}
